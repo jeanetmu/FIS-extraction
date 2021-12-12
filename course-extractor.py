dbfac5d7102442957ad7a798344ce72df6310bc3
jeanetmu <jeanmurs@gmail.com>
2021-12-12 16:53:56 +0100

    updated text-strings for each method in course-extractor.py

--- course-extractor.py
+++ course-extractor.py
@@ -2,7 +2,6 @@ import requests as req
 import regex as re
 import urllib.request
 import os
-import cv2
 
 
 def get_html(url, params=None, output=None, ending=".txt"):
@@ -15,6 +14,8 @@ def get_html(url, params=None, output=None, ending=".txt"):
             url: String referring to wanted Wikipedia-article
             (optional) params: dictionary over possible parameters for req.get()
             (optional) output: String file name for output storing of HTML data
+            (optional) ending: ".txt" as standard ending for potential output-file,
+                                could also be ex: ".html"
 
     """
     #link = "https://"+url
@@ -29,28 +30,24 @@ def get_html(url, params=None, output=None, ending=".txt"):
     if not output:
         return resp.text
 
-    out = open(str(output)+ending, "w") # saving output in file (.txt when unspecified)
+    # saving output in file
+    out = open(str(output)+ending, "w")
     out.write(resp.text)
 
-def find_courses(url="",fn="",output="partial-urls.txt"):
+
+def find_courses(url="",fn="",output=""):
     """
-    Takes an html page and finds all present images
+    Finds all partial urls (courses) in a FIS-page table of information, saves
+    all found partial urls in an output-file for further crawling and extraction
 
     Args:
-            html: html-file
+            (optional) url: potential url for course crawling/finding (not really used per now)
+            (optional) fn: potential filename for course crawling/finding
+            (optional) output: filename for wanted output-file (for storing partial urls)
     Returns:
-            A list of all found courses
+            -- prints what files have been updated with newly found partial urls --
     """
 
-    #Attempt with BeautifulSoup
-    # response = req.get(fn)
-    # soup = bs4.BeautifulSoup(response.text, 'lxml')
-    # soup = bs4.BeautifulSoup(open("nor_courses.txt"),'txt')
-    # code = soup.find_all('div',{'table-row pointer reset-padding'})
-    # c2 = code.find_all('onclick')
-    # print(c2)
-
-    #Attempt with regex
     text = open(fn).read()
     out = open(output, 'a')
 
@@ -68,17 +65,29 @@ def find_courses(url="",fn="",output="partial-urls.txt"):
 
     print("updated \""+output+"\"-file --")
 
-def get_pdfs(fn,base,folder="Misc/"out="Misc/",url="https://medias2.fis-ski.com/pdf/homologations"): #url is pointer to pdf-portal
+def get_pdfs(fn,base,folder="Misc/"out="Misc/",url="https://medias2.fis-ski.com/pdf/homologations"):
+    """
+    First gets all new html-pages as .txt-files for crawling. Then iterates over each page
+    and extracts the wanted course-pdf-files with the "correct" (already used) information
+    from course-pdf-url filename for pdf upon saving.
+
+    Args:
+            fn: filename of file with all partial urls
+            base: base-path for what will become the full urls (base + partial-url)
+            (optional) folder: potential folder for storing crawled .txt/.html-pages for each destination
+            (optional) out: potential folder for storing actual crawled pdfs at end of call
+            (optional) url: found common baseline url for each of the course-pdfs (can be changed if needed)
+    """
 
     file = open(fn, 'r')
     courses = file.readlines()
 
-    # #first getting all the needed htmls as txt-files
-    # for course in courses:
-    #     url = str(base+str(course))
-    #     name = re.findall(r'(?<=place=).*[A-Z].*(?=\&disciplinecode\=)', url)
-    #     if name: #potential for normalization of names here "%2C"="," og "+"=" "
-    #         get_html(url,output=folder+str(name[0])) #saving all files in the chosen folder
+    #First getting all the needed htmls as txt-files
+    for course in courses:
+        url = str(base+str(course))
+        name = re.findall(r'(?<=place=).*[A-Z].*(?=\&disciplinecode\=)', url)
+        if name: #potential for normalization of names here "%2C"="," og "+"=" "
+            get_html(url,output=folder+str(name[0])) #saving all files in the chosen folder
 
     #Then moving on to actual extraction
     pattern = r'(?<=onclick\=\"fct_download_certificatepdf\().*(?=\)\;)'
@@ -98,19 +107,16 @@ def get_pdfs(fn,base,folder="Misc/"out="Misc/",url="https://medias2.fis-ski.com/
                         path += "/" + e
                         if e != "CC" and e != "NOR":
                             name = e
-                    print(path)
+                    print(path) #prints url of what course we are currently working on extracting
                     dst = os.path.join(out, name)
                     urllib.request.urlretrieve(path, dst)
 
 
-
-
-
 if __name__ == "__main__":
     # html_file = get_html("https://www.fis-ski.com/DB/cross-country/homologations.html",output="courses-FIS")
-    new_url = "https://www.fis-ski.com/DB/cross-country/homologations.html?sectorcode=CC&nationcode=nor"
-    # get_html(new_url, output="nor_courses")
 
-    #Actual runthrough of code as of now
-    #find_courses(url=new_url, fn="nor_courses.txt")
-    get_pdfs("partial-urls.txt","https://www.fis-ski.com/DB/cross-country/homologations.html?place=", folder="NOR-courses/",out="NOR-pdfs/")
+    ## First runthrough of code - for Norwegian course extraction ##
+    # new_url = "https://www.fis-ski.com/DB/cross-country/homologations.html?sectorcode=CC&nationcode=nor"
+    # get_html(new_url, output="nor_courses")
+    # find_courses(url=new_url, fn="nor_courses.txt", output="partial-urls.txt")
+    # get_pdfs("partial-urls.txt","https://www.fis-ski.com/DB/cross-country/homologations.html?place=", folder="NOR-courses/",out="NOR-pdfs/")
